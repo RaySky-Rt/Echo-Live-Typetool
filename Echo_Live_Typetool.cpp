fa8aeb8f2dcd9e80b574b48b51b6d045ea537e86
RaySky_Rt <raysky_rt@foxmail.com>
2023-12-19 05:00:33 +0800

    更新gitignore文件，优化代码并添加一些功能（未完成
    更新gitignore文件
    规范了代码中的变量命名
    添加一些功能（未完成

--- Echo_Live_Typetool.cpp
+++ Echo_Live_Typetool.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include <conio.h>
 
 using namespace std;
 
+void refreshConsole(); //清屏
+void command_execute(string command); //执行指令
+void read_config(); //读取配置文件
+void show_commandlist(); //输出指令列表
+string getcommand(string a); //接收指令
+
 int main() {
+    string commandlist[101],userInput;
     ofstream file("start.js", ios::out);
 
     if (!file.is_open()) {
@@ -12,20 +20,27 @@ int main() {
     }
 
     while (true) {
-        string a;
+        userInput=""; //清空userInput
 
-        cout << "请输入文本...？ (输入\"exit\"来退出程序): ";
-        getline(cin, a);
+        cout << "请输入文本...？ (输入exit退出程序): ";
 
-        if (a == "exit") {
+        getline(cin, userInput);
+
+        if (userInput=="exit") {
+
             break;
-        }
+
+        }else if(userInput[0]=='/'){//指令
+
+            getcommand(userInput);
+
+        }else{
         file.open("start.js", ios::out | ios::trunc);
         file << "const data = {\n"
              "    \"username\": \".\", \n"
              "    \"messages\": [\n"
              "        {\n"
-             "            \"message\": \"" << a << "\",\n"
+             "            \"message\": \"" << userInput << "\",\n"
              "            \"data\": {\n"
              "                \"printSpeed\": \"30\"\n"
              "            }\n"
@@ -34,8 +49,37 @@ int main() {
              "}\n";
         file.close();
         cout << "文本更新完成！\n";
+        }
     }
     
     file.close();
     return 0;
+}
+
+void refreshConsole(){
+    system("cls"); //控制台清屏（Windows平台）
+}
+
+void command_execute(string command){
+    if(command==""){
+
+    }
+}
+
+void read_config(){
+
+}
+
+void show_commandlist(){
+
+}
+
+string getcommand(string userInput){
+    string command;
+    int input_size;
+    input_size=userInput.size();
+    for(int i=0;i<input_size;i++){
+        command[i]=userInput[i+1];
+    }
+    return command;
 }
