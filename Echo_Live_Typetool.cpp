a7cb19289835f8d967ddcbb46a1b7db9023dfe7b
RaySky_Rt <raysky_rt@foxmail.com>
2024-01-12 18:04:17 +0800

    实装了指令系统，优化使用体验
    实装指令系统，新增了两个指令/name 和/clear ，稍微优化了一下使用体验

--- Echo_Live_Typetool.cpp
+++ Echo_Live_Typetool.cpp
@@ -8,12 +8,12 @@ void refreshConsole(); //清屏
 void command_execute(string command); //执行指令
 void read_config(); //读取配置文件
 void show_commandlist(); //输出指令列表
-string getcommand(string a); //接收指令
+string getcommand(string a); //解析指令
 
 //上述写好的几个函数暂未正式投入使用（
-
+string username;
 int main() {
-    string commandlist[101],userInput,username;
+    string commandlist[101],userInput;
     ofstream file("start.js", ios::out);
 
     if (!file.is_open()) {
@@ -21,9 +21,8 @@ int main() {
         return 1;
     }
 
-    cout<<"欢迎使用————请输入想要显示的用户名称吧！";
-    
-    cin>>username;
+    cout<<"欢迎使用！请输入想要显示的用户名~"<<endl;
+    getline(cin, username);
 
     while (true) {
         userInput=""; //清空userInput
@@ -36,11 +35,11 @@ int main() {
 
             break;
 
-        }/*else if(userInput[0]=='/'){//指令
+        }else if(userInput[0]=='/'){//指令
 
-            command_execute(getcommand(userInput));
+            command_execute(userInput);
 
-        }*/else{
+        }else{
         file.open("start.js", ios::out | ios::trunc);
         file << "echolive.send({\n"
              "    \"username\": \"" << username << "\", \n"
@@ -54,7 +53,7 @@ int main() {
              "    ]\n"
              "});\n";
         file.close();
-        cout << "文本更新完成！\n";
+        cout << "文本消息发送成功！\n";
         }
     }
     
@@ -66,14 +65,19 @@ void refreshConsole(){
     system("cls"); //控制台清屏（Windows平台）
 }
 
-void command_execute(string command){
-    // if(command=="name"){
-    //     string input;
-    //     cin>>input;
-    //     username=input;
-    //     cout<<"显示名称更改成功！"<<endl;
-    //     return;
-    // }
+void command_execute(string userInput){
+    string command;
+    command=getcommand(userInput);
+    if(command=="name"){
+        string input;
+        input=userInput.substr(6);
+        username=input;
+        cout<<"已将说话人名称改为"<<username<<endl;
+        return;
+    }else if(command=="clear"){
+        refreshConsole();
+        return;
+    }
     return;
 }
 
@@ -87,10 +91,15 @@ void show_commandlist(){
 
 string getcommand(string userInput){
     string command;
-    int input_size;
+    size_t input_size;
+    size_t spacePos = userInput.find(' ', 1); //找到指令后第一个空格的位置
     input_size=userInput.size();
-    for(int i=0;i<input_size;i++){
-        command[i]=userInput[i+1];
-    }
+    if (spacePos != string::npos) {
+        command = userInput.substr(1, spacePos-1);
+            return command;
+        }else{
+            command = userInput.substr(1);
+            return command; //没检测到指令的参数，斜杠后的文本即为指令，直接返回
+        }
     return command;
 }
