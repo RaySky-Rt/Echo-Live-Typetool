4200c3513641a26bd4eb5bb10a2921b2716da298
RaySky_Rt <raysky_rt@foxmail.com>
2024-01-13 03:50:57 +0800

    v0.4更新

--- Echo_Live_Typetool.cpp
+++ Echo_Live_Typetool.cpp
@@ -1,43 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <conio.h>
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::ordered_json;
 using namespace std;
 
 void refreshConsole(); //清屏
 void command_execute(string command); //执行指令
 void read_config(); //读取配置文件
+void write_config(); //写入配置文件
 void show_commandlist(); //输出指令列表
 string getcommand(string a); //解析指令
 
-//上述写好的几个函数暂未正式投入使用（
-string username,prefix,suffix;
+string username,prefix,suffix,printSpeed="30";
+json config;
+//一些全局变量
+
 int main() {
     string commandlist[101],userInput;
-    ofstream file("start.js", ios::out);
 
+    read_config();
+
+    ofstream file("start.js", ios::out);
     if (!file.is_open()) {
         cerr << "无法打开start.js，请检查start.js是否被其他程序占用\n";
         return 1;
     }
-
-    cout<<"欢迎使用！请输入想要显示的用户名~"<<endl;
-    getline(cin, username);
+    // if(username==""){
+    //     cout<<"欢迎使用！请输入想要显示的用户名~"<<endl;
+    //     getline(cin, username);
+    // }
 
     while (true) {
         userInput=""; //清空userInput
 
-        cout << "请输入文本...？ (输入exit退出程序): ";
+        cout << "请输入文本...？: ";
 
         getline(cin, userInput);
 
-        if (userInput=="exit") {
-
-            break;
-
-        }else if(userInput[0]=='/'){//指令
-
-            command_execute(userInput);
+        if(userInput[0]=='/'){//指令
+            if(userInput=="/exit"){
+                break;
+            }else{
+                command_execute(userInput);
+            }
 
         }else{
         file.open("start.js", ios::out | ios::trunc);
@@ -47,7 +54,7 @@ int main() {
              "        {\n"
              "            \"message\": \"" << prefix << userInput << suffix << "\",\n"
              "            \"data\": {\n"
-             "                \"printSpeed\": \"30\"\n"
+             "                \"printSpeed\": \"" << printSpeed << "\"\n"
              "            }\n"
              "        }\n"
              "    ]\n"
@@ -62,7 +69,7 @@ int main() {
 }
 
 void refreshConsole(){
-    system("cls"); //控制台清屏（Windows平台）
+    system("cls"); // 控制台清屏（仅限Windows平台）
 }
 
 void command_execute(string userInput){
@@ -80,6 +87,8 @@ void command_execute(string userInput){
             username=input;
             cout<<"已将说话人名称改为"<<username<<endl;
         }
+        write_config();
+        read_config();
         return;
     }else if(command=="clear"){
         refreshConsole();
@@ -100,6 +109,8 @@ void command_execute(string userInput){
                 cout<<"已将前引用符号改为"<<prefix<<endl;
             }
         }
+        write_config();
+        read_config();
     }else if(command=="suffix"){
         string input;
         if(userInput.size()<=8){
@@ -116,6 +127,26 @@ void command_execute(string userInput){
                 cout<<"已将后引用符号改为"<<suffix<<endl;
             }
         }
+        write_config();
+        read_config();
+    }else if(command=="printspeed"){
+        string input;
+        if(userInput.size()<=12){
+            cout<<"参数不存在，请重试"<<endl;
+        }else{
+            input=userInput.substr(12);
+            printSpeed=input;
+            cout<<"已将打印速度设置为"<<printSpeed<<endl;
+        }
+        write_config();
+        read_config();
+    }else if(command=="readconfig"){
+        read_config();
+        cout<<"配置文件读取成功！"<<endl;
+        return;
+    }else{
+        cout<<"无效指令，请重试！"<<endl;
+        return;
     }
 
     return;
@@ -123,10 +154,59 @@ void command_execute(string userInput){
 
 void read_config(){
 
+    ifstream configFile("typetool_config.json");
+    if (!configFile.is_open()) {
+        cerr << "无法打开配置文件 typetool_config.json\n";
+        return;
+    }
+    // 读取文件内容到 JSON 对象
+    configFile >> config;
+    // 关闭文件
+    configFile.close();
+    // 检查是否成功读取配置
+    if (config.empty()) {
+        cerr << "配置文件为空或格式错误\n";
+        return;
+    }
+
+    if (config["username"].type() == json::value_t::string) {
+        username = config["username"];
+    }
+    if (config["prefix"].type() == json::value_t::string) {
+        prefix = config["prefix"];
+    }
+    if (config["suffix"].type() == json::value_t::string) {
+        suffix = config["suffix"];
+    }
+    if (config["printSpeed"].type() == json::value_t::string) {
+        printSpeed = config["printSpeed"];
+    }
+
+    return;
+}
+
+void write_config(){
+
+    config["username"] = username;
+    config["prefix"] = prefix;
+    config["suffix"] = suffix;
+    config["printSpeed"] = printSpeed;
+
+    ofstream configFile("typetool_config.json");
+
+    if (!configFile.is_open()) {
+        cerr << "无法打开配置文件 typetool_config.json\n";
+        return;
+    }
+
+    configFile << setw(4) << config << endl;
+    configFile.close();
+
+    return;
 }
 
 void show_commandlist(){
-
+    //还没写，这个函数真的用得上吗（
 }
 
 string getcommand(string userInput){
